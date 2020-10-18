7ac5e05d61a162d49470ef8017b5e0b5763effd4
BalthPa <balthazarpapin@gmail.com>
2020-10-18 18:37:52 +0200

    Add put, delete, post, get

--- user.php
+++ user.php
@@ -5,4 +5,16 @@ require "general.php";
 class User extends General{
 
     protected $table = "user";
+
+    public function save($param){
+        $statement = "INSERT INTO user (nom, prenom, email, password, role)
+                        VALUES (:nom, :prenom, :email, :password, :role)";
+
+        $param["nom"]= htmlspecialchars($param["nom"]);
+        $param["prenom"]= htmlspecialchars($param["prenom"]);
+        $param["password"]= password_hash($param["password"], PASSWORD_DEFAULT);
+        $param["role"]= json_encode(["ROLE_USER"]);
+
+        $this->db->prepare($statement, 'save', $param);
+    }
 }
